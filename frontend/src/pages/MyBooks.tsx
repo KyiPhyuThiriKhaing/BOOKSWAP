3cda8ad494d2736e5f1cf8ff3afacf5d616db345
Kyi Phyu Thiri Khaing <kyiphyuthirikhaing@gmail.com>
2026-06-19 12:44:33 +0700

    feat: create my books page

--- frontend/src/pages/MyBooks.tsx
+++ frontend/src/pages/MyBooks.tsx
@@ -1,8 +1,26 @@
 function MyBooks() {
+  const myBooks = [
+    {
+      id: 1,
+      title: "Atomic Habits",
+    },
+    {
+      id: 2,
+      title: "Clean Code",
+    },
+  ];
+
   return (
     <div>
       <h1>My Books</h1>
-      <p>Manage your books here.</p>
+
+      <button>Add Book</button>
+
+      {myBooks.map((book) => (
+        <div key={book.id}>
+          <h3>{book.title}</h3>
+        </div>
+      ))}
     </div>
   );
 }
