e73b31ea23cf008da9d3f6e2f23a98053932ef08
Kyi Phyu Thiri Khaing <kyiphyuthirikhaing@gmail.com>
2026-06-19 12:44:12 +0700

    feat: create registration form

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -2,7 +2,34 @@ function Register() {
   return (
     <div>
       <h1>Register</h1>
-      <p>Create a new account.</p>
+
+      <form>
+        <div>
+          <label>Name</label>
+          <br />
+          <input type="text" />
+        </div>
+
+        <br />
+
+        <div>
+          <label>Email</label>
+          <br />
+          <input type="email" />
+        </div>
+
+        <br />
+
+        <div>
+          <label>Password</label>
+          <br />
+          <input type="password" />
+        </div>
+
+        <br />
+
+        <button type="submit">Register</button>
+      </form>
     </div>
   );
 }
